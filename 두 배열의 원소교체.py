ca0d2616fe9a7438c446a146032b8defa14ac734
ccccljs <cho38suk@hamail.net>
2022-01-05 10:05:40 +0900

    배열의 원소교체

--- 두 배열의 원소교체.py
+++ 두 배열의 원소교체.py
@@ -1,21 +1,14 @@
-'''
 n,k = map(int,input().split())
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-'''
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
 
-n = 5
-k = 3
-a = [1, 2, 5, 4, 3]
-b = [5, 5, 6, 6, 5]
-
-a.sort() # a에 대해서 오름차순으로 정렬
-b.sort(reverse=True) #b에 대해서 내림차순으로 정렬
+a.sort() # 오름차순 정렬
+b.sort(reverse=True) # 내림차순 정렬
 
 for i in range(k):
-    if a[i] < b[i]:
-        a[i], b[i] = b[i], a[i] # 원소교체
+    if a[i] < b[i]: # a의 원소가 b의 원소 보다 작다면
+        a[i] , b[i] = b[i] , a[i]
     else:
         break
-
 print(sum(a))
+
